e1bc655a7047a596e2c85e19540558cade69cd13
Teo <theodoroskourtalisgithub@gmail.com>
2024-12-20 23:25:56 +0200

    Update 01-Fetch_Raw_Data.py

--- pages/01-Fetch_Raw_Data.py
+++ pages/01-Fetch_Raw_Data.py
@@ -73,9 +73,6 @@ def main():
     st.markdown("---")
     st.markdown("### Select a Step:")
     
-    # Navigation Buttons with Correct Page Titles
-    if st.button("Step 1: Fetch Raw Data"):
-        switch_page("fetch raw data")
     
     if st.button("Step 2: Clean Data"):
         switch_page("clean data")
